d08ce716aaf9d2b83818c804913eb1bd7f6603a4
Tomas Arano Loxton <tomasaralox@gmail.com>
2024-04-27 20:35:44 +0200

    Added basic templates
    Added the base template and child template

--- DataA/FlaskApp/board/__init__.py
+++ DataA/FlaskApp/board/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+    return app
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+
+
+# python -m flask --app board run --port 8000 --debug
