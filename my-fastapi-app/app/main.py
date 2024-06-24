33ed08a38b19318d919491250372ea46d06bb1e6
Tomas Arano Loxton <tomasaralox@gmail.com>
2024-06-24 11:30:14 +0900

    Add Pydantic model for Flashcards
    This commit introduces a Pydantic model for Flashcards, including an Enum for FlashcardType.

--- my-fastapi-app/app/main.py
+++ my-fastapi-app/app/main.py
@@ -1,10 +1,59 @@
-from fastapi import FastAPI
-from api import routes
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import  List, Optional, Annotated
+import models
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from uuid import UUID, uuid4 #i dont need this?
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
+## Pydantic models  
+from pydantic import BaseModel
+from enum import Enum
+
+class FlashcardType(str, Enum):
+    kanji = "kanji"
+    vocab = "vocab"
+    radical = "radical"
+
+class FlashcardBase(BaseModel):
+    id: int
+    level: int
+    type: FlashcardType
+    fields: str
+
+    class Config:
+        orm_mode = True
+## Pydantic schemas END
+
+
+#Method get_db
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+#Method get_db END
+
+## API ENDPOINTS
+# READ 10 flashcards
+
+@app.get("/flashcards", response_model=List[models.Flashcard])
+def read_flashcards(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    flashcards = db.query(models.Flashcard).offset(skip).limit(limit).all()
+    return flashcards
+
+
+
+'''
 app.include_router(routes.router)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+'''
---
+++ my-fastapi-app/app/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Enum
+
+Base = declarative_base()
+
+# Enum for flashcard types 
+class FlashcardType(PyEnum):
+    kanji = "kanji"
+    vocab = "vocab"
+    radical = "radical"
+# Flashcards models
+class Flashcard(Base):
+    __tablename__ = "flashcards"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    level = Column(Integer, index=True)
+    type = Column(Enum(FlashcardType), index=True)
+    fields = Column(String(255))
+
+#User SQLAlchemy model for users
+'''
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(16), unique=True, index=True)
+    password = Column(String(64))
+    email = Column(String(64), unique=True, index=True)
+    created_at = Column(DateTime)
+'''
